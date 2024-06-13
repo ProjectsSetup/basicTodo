567e6617d18dc6d03103bea9605db43951962fa0
8763625 <ksingh3625@conestogac.on.ca>
2024-06-13 14:16:30 -0400

    The database update and put endpoint created

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
 //write basic express boilerplate code
 //with express.json() middleware
-
+//importing dependencies for the appication
 const express = require("express");
 const jsonwebtoken = require("jsonwebtoken");
 const { createTodo, updateTodo } = require("./types");
+const { todo } = require("./db");
 
 
 const app = express();
@@ -11,7 +12,7 @@ const app = express();
 app.use(express.json());
 
 //post endpoint to add a todo after validating inputs given using zod
-app.post("/todo", function(req, res) {
+app.post("/todo", async function(req, res) {
     const createPayload = req.body;
     const parsedPayload = createTodo.safeParse(createPayload);
     if(!parsedPayload.success) {
@@ -20,15 +21,40 @@ app.post("/todo", function(req, res) {
         })
         return;
     }
+
+    //add it in the mongodb database in a try catch block
+    try {
+        //wait for the database call and todo creation before responding to the user
+        await todo.create({
+            title: createPayload.title,
+            description: createPayload.description,
+            completed: false
+        })
+        res,json({
+            msg: "Todo created"
+        })
+    } catch (error) {
+        //response sent back to the user in case an error occured while creating a todo
+        res.json({
+            msg: "error occured, process not completed"
+        })
+    }
+    
+    
 })
 
-//get endpoint to get todos after authenticating user
-app.get("/todos", function(req, res) {
+//get endpoint to get todos for the user
+app.get("/todos", async function(req, res) {
+    const todos = await todo.find({});
+    //send back the todos to the user
+    res.json({
+        todos
+    })
     
 })
 
 //endpoint to complete a todo after validating inputs given using zod libaray
-app.put("/completed", function(req, res) {
+app.put("/completed", async function(req, res) {
     const updatePayload = req.body;
     const parsedPayload = updateTodo.safeParse(updatePayload);
     if(!parsedPayload.success) {
@@ -37,10 +63,18 @@ app.put("/completed", function(req, res) {
         })
         return;
     }
-    //add mongodb code to handle inputs here
+    // now handling the database to make the todo as completed 
+    await todo.update({
+        _id: req.body.id
+    }, {
+        completed: true
+    })
+    res.json({
+        msg: "Todo marked as Completed"
+    })
 })
 
 //endpoint to delete a todo after authorizing the user
 app.delete("/todo", function(req, res) {
-
+    
 })
