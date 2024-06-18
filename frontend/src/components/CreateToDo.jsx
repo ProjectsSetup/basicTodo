3ea6b1e09068025dbe15caa5eadb4c66707c24b5
8763625 <ksingh3625@conestogac.on.ca>
2024-06-18 17:50:21 -0400

    added basis frontend in React

---
+++ frontend/src/components/CreateToDo.jsx
@@ -0,0 +1,10 @@
+export function CreateToDo() {
+    return <div> 
+        <input type="text" placeholder="title"></input>
+        <input type="text" placeholder="description"></input>
+
+        <button>Add a Todo</button>
+    </div>
+}
+
+
